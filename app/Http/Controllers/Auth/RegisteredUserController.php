e99c5cbbb3998addd9783ac4b5c52d1b22c6814c
unknown <ujjalroy1011@gmail.com>
2025-08-21 02:05:08 +0600

    setup done

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\View\View;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+
+        //generate account id
+    private function generateNextAccountId()
+    {
+        do {
+            $randomAccountId = rand(10000000, 99999999);
+    
+            $exists = User::where('account_id', $randomAccountId)->exists();
+    
+        } while ($exists); 
+    
+        return $randomAccountId;
+    }
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+        $accountId = $this->generateNextAccountId();//call the account_id
+
+        $user = User::create([
+            'account_id' => $accountId,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(route('dashboard', absolute: false));
+    }
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('user.index');
+    }
+    
+    public function admin_index()
+    {
+        return view('admin.index');
+    }
+}
